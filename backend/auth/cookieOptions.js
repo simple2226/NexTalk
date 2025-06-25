567fbe6c14b5a48bb68e52c9f233314e827f6b67
Rohit Khallar <iec2023099@iiita.ac.in>
2025-06-25 18:32:35 +0530

    modified:   backend/auth/cookieOptions.js

--- backend/auth/cookieOptions.js
+++ backend/auth/cookieOptions.js
@@ -2,7 +2,7 @@ const cookieOptions = {
     httpOnly: true, 
     secure: true,
     sameSite: 'None',
-    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
+    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) 
 }
 
 
